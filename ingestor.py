266d3b864a7c8065f73769dc6f5c0b3365c37a18
Shubham Thakur <sdthakur@iu.edu>
2022-02-03 20:58:04 -0500

    Check for local file before reaching out to s3

--- ingestor.py
+++ ingestor.py
@@ -1,38 +1,57 @@
 import pyart
+from os.path import exists
 from matplotlib import pyplot as plt
-import nexradaws
 from datetime import datetime
+import nexradaws
 
-conn = nexradaws.NexradAwsInterface()
+def get_scans(conn, year, month, day, radar_id):
+    return conn.get_avail_scans(year, month, day, radar_id)
 
-availscans = conn.get_avail_scans('2021', '05', '31', 'KTLX')
-print("There are {} NEXRAD files available for May 31st, 2021 for the KTLX radar.\n".format(len(availscans)))
+def save_file(conn, year, month, day, radar_id):
+    filename = f"{year}-{month}-{day}-{radar_id}.png"
 
-#print(availscans[0:4])
-considered_scans = []
-considered_scans.append(availscans[0])
-considered_scans.append(availscans[89])
-considered_scans.append(availscans[178])
-considered_scans.append(availscans[326])
-res = conn.download(considered_scans, './')
+    # a plot is unique for a combination of date and radar_id
+    if exists(filename):
+        return filename
 
-print(res.success)
+    avail_scans = conn.get_avail_scans(year, month, day, radar_id)
 
-fig = plt.figure(figsize=(16,12))
-for i,scan in enumerate(res.iter_success(),start=1):
+    # ignore MDM files
+    avail_scans = [scan for scan in avail_scans if scan.filename.find('MDM') == -1]
 
-    #print(f"{scan.radar_id} {scan.scan_time}")
-    #continue
+    # Compute indices that will be used to generate plots
+    quartile_index = len(avail_scans) // 4
+    three_quartile_index = quartile_index * 3
 
-    ax = fig.add_subplot(2,2,i)
-    radar = scan.open_pyart()
-    display = pyart.graph.RadarDisplay(radar)
-    display.plot('reflectivity', 0, title=f"{scan.radar_id} {scan.scan_time}",
-                vmin=-32, vmax=64, colorbar_label='', ax=ax)
-    display.plot_range_ring(radar.range['data'][-1]/1000., ax=ax)
-    display.set_limits(xlim=(-500, 500), ylim=(-500, 500), ax=ax)
+    considered_scans = []
+    considered_scans.append(avail_scans[0])
+    considered_scans.append(avail_scans[quartile_index])
+    considered_scans.append(avail_scans[three_quartile_index])
+    considered_scans.append(avail_scans[len(avail_scans) - 1])
+    res = conn.download(considered_scans, './')
 
-plt.savefig('p.png')
+    print(res.success)
+
+    fig = plt.figure(figsize=(16,12))
+
+    times = ['After midnight', 'Early morning', 'Evening', 'Late Night']
+
+    for i,scan in enumerate(res.iter_success(),start=1):
+        ax = fig.add_subplot(2,2,i)
+        radar = scan.open_pyart()
+        display = pyart.graph.RadarDisplay(radar)
+        display.plot('reflectivity', 0, title=f"{scan.radar_id} {scan.scan_time} ({times[i - 1]})",
+                    vmin=-32, vmax=64, colorbar_label='', ax=ax)
+        display.plot_range_ring(radar.range['data'][-1]/1000., ax=ax)
+        display.set_limits(xlim=(-500, 500), ylim=(-500, 500), ax=ax)
+
+    plt.savefig(filename)
+
+    return filename
+
+if __name__ == "__main__":
+    conn = nexradaws.NexradAwsInterface()
+    save_file(conn, '2018', '06', '07', 'KTLX')
 
 
 
